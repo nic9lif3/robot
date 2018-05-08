db53deaf48247016d70e8e8205706eeaa4277aef
Nam Nguyen <nam.igenta@gmail.com>
2018-05-08 15:46:32 +0700

    a

--- control.py
+++ control.py
@@ -6,27 +6,27 @@ pos=np.argwhere(maze==2)[0]
 end=np.argwhere(maze==3)[0]
 treasure=np.argwhere(maze==4)
 
-direction=[0,1]
+direction=np.array([-1,0])
 
 def left():
-	if direction==[0,-1]:
-		return [-1,0]
-	if direction==[-1,0]:
-		return [0,1]
-	if direction==[0,1]:
-		return [1,0]
-	if direction==[1,0]:
-		return [0,-1]
+	if all(direction==[-1,0]):
+		return np.array([0,-1])
+	if all(direction==[0,-1]):
+		return np.array([1,0])
+	if all(direction==[1,0]):
+		return np.array([0,1])
+	if all(direction==[0,1]):
+		return np.array([-1,0])
 
 def right():
-	if direction==[0,-1]:
-		return [1,0]
-	if direction==[1,0]:
-		return [0,1]
-	if direction==[0,1]:
-		return [-1,0]
-	if direction==[-1,0]:
-		return [0,-1]	
+	if all(direction==[-1,0]):
+		return np.array([0,1])
+	if all(direction==[0,1]):
+		return np.array([1,0])
+	if all(direction==[1,0]):
+		return np.array([0,-1])
+	if all(direction==[0,1]):
+		return np.array([-1,0])
 
 def back_to_check_point(road):
 	way='ll'
@@ -39,5 +39,15 @@ def back_to_check_point(road):
 			way+=l
 	return way
 
+def check_wall():
+	return np.array([maze[pos[0]+direction[0]][pos[1]+direction[1]],maze[pos[0]+left()[0]][pos[1]+left()[1]],maze[pos[0]+right()[0]][pos[1]+right()[1]]])==-1 
+	# phia truoc, ben trai, ben phai
+def check_out():
+	return np.array([maze[pos[0]+direction[0]][pos[1]+direction[1]],maze[pos[0]+left()[0]][pos[1]+left()[1]],maze[pos[0]+right()[0]][pos[1]+right()[1]]])==3
+	# phia truoc, ben trai, ben phai
 
+print(check_wall())
+print(check_out())
 
+def go(first_turn=None):
+	pass
