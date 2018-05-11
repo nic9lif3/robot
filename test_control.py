3a20bd3c186a15bb250d8f3275e4724958f35394
Nam Nguyen <nam.igenta@gmail.com>
2018-05-11 11:25:42 +0700

    a

--- test_control.py
+++ test_control.py
@@ -71,16 +71,6 @@ def update_maze_to_tmp_maze():
 def update_tmp_maze():
     global tmp_maze
     tmp_maze[pos[0]][pos[1]] = 9
-    # show(tmp_maze)
-    if all(direction == [-1, 0]):
-        tmp='up'
-    if all(direction == [0, -1]):
-        tmp='left'
-    if all(direction == [1, 0]):
-        tmp='down'
-    if all(direction == [0, 1]):
-        tmp='right'
-    print(pos,tmp)
     if pos[0] + direction[0] != 0 and pos[0] + direction[0] != 10 and pos[1] + direction[1] != 0 and pos[1] + direction[1] != 10:
         tmp_maze[pos[0] + direction[0] * 2][pos[1] + direction[1] * 2] = 5
     # if pos[0] + left()[0] != 0 and pos[0] + left()[0] != 10 and pos[1] + left()[1] != 0 and pos[1] + left()[1] != 10:
@@ -96,30 +86,45 @@ def check_wall():
                      maze[pos[0] + right()[0]][pos[1] + right()[1]]]) == -1
 
 
-# phia truoc, ben trai, ben phai
+# hien thi vi tri hien tai
+def show_pos():
+    if all(direction == [-1, 0]):
+        tmp = 'up'
+    if all(direction == [0, -1]):
+        tmp = 'left'
+    if all(direction == [1, 0]):
+        tmp = 'down'
+    if all(direction == [0, 1]):
+        tmp = 'right'
+    print(pos, tmp)
 
 # ham check phia truoc, trai, phai co phai loi ra khong
 def check_out():
     return np.array([maze[pos[0] + direction[0]][pos[1] + direction[1]], maze[pos[0] + left()[0]][pos[1] + left()[1]],
                      maze[pos[0] + right()[0]][pos[1] + right()[1]]]) == 3
 
+def check_if_pass():
+    return imagine_maze[pos_imagine[0] + direction[0]][pos_imagine[1] + direction[1]]==-1
 
 # phia truoc, ben trai, ben phai
 
 # ham thuc hien viec di chuyen
 def process(way):
-    global pos, direction, tmp_maze,all_step
+    global pos, direction, tmp_maze,all_step,pos_imagine
     for i in way:
-        print(i,end=' ')
+        print(str(i),end=' ')
         update_maze_to_tmp_maze()
         if i == 'f':
             pos += direction * 2
+            pos_imagine+=direction * 2
+            imagine_maze[pos_imagine[0]][pos_imagine[1]] = -1
         elif i == 'l':
             direction = left()
         elif i == 'r':
             direction = right()
         elif i=='b':
             pos-=direction * 2
+        show_pos()
         update_tmp_maze()
         all_step+=i
 
@@ -142,7 +147,11 @@ def go(road):
     wall = check_wall()
     for i in np.random.choice(3, 3, False):
         if not wall[i]:
-            t1, t2 = go(name[i] + 'f')
+            process(name[i])
+            if check_if_pass():
+                process(come_back(name[i]))
+                continue
+            t1, t2 = go('f')
             if t1 == True:
                 return True, road + t2
     print('Come back '+come_back(road))
@@ -150,16 +159,22 @@ def go(road):
     print('End come back')
     return False, None
 
+def init():
+    global imagine_maze,pos_imagine,pos,direction,tmp_maze
+    imagine_maze=np.zeros((30,30))
+    direction = np.array([-1, 0])
+    pos_imagine=np.array([15,15])
+    pos = np.argwhere(maze == 9)[0]
+    tmp_maze = np.array(maze)
 
-global pos, direction, tmp_maze,all_step,imagine_maze
+
+
+global pos, direction, tmp_maze,all_step,imagine_maze,pos_imagine
 all_step=''
-imagine_maze=np.zeros((21,21))
-pos = np.argwhere(maze == 9)[0]
-end = np.argwhere(maze == 3)[0]
-name = ['', 'l', 'r', 'rr']
-direction = np.array([-1, 0])
-tmp_maze = np.array(maze)
+init()
 show(maze)
+show_pos()
+name = ['', 'l', 'r', 'rr']
 wall = np.append(check_wall(), maze[pos[0] + back()[0]][pos[1] + back()[1]] == -1)
 
 if any(np.append(check_out(),maze[pos[0] + back()[0]][pos[1] + back()[1]] == 3)):
@@ -173,9 +188,6 @@ for i in np.random.choice(4, 4, False):
             print(t2)
             break
 
-tmp_maze=maze
-pos = np.argwhere(maze == 9)[0]
-direction = np.array([-1, 0])
 fig, ax = plt.subplots(figsize=(5, 8))
 
 def update(i):
@@ -184,8 +196,7 @@ def update(i):
     ax.set_title("Step: {}".format(i), fontsize=20)
     ax.set_axis_off()
 
-def init():
-    pass
+
 anim = FuncAnimation(fig, update, frames=range(len(all_step)), interval=800,init_func=init)
 anim.save('step.gif', dpi=80)
 plt.close()
